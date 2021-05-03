d235c4b53d9cfe562e1fbe43022ad01247a2dfb8
Sidi00X <sidritdemiraj1@gmail.com>
2021-05-03 15:43:33 +0100

    Added Event Listener with JS

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let pl_score = 0;
+let cm_score = 0;
+
 const playRound = player => {
     const computerSelection = () => {
         var x = Math.floor((Math.random() * 3) + 1);
@@ -23,27 +26,49 @@ const playRound = player => {
     else if (player == 'rock') {
         if (computer == 'paper') {
             document.getElementById('result').innerHTML = 'You lost!';
+            cm_score++;
         }
         else if (player == 'scissors') {
             document.getElementById('result').innerHTML = 'You won';
+            pl_score++;
         }
     }
 
     else if (player == 'paper') {
         if (computer == 'rock') {
             document.getElementById('result').innerHTML = 'You won!';
+            pl_score++;
         }
         else if (player == 'scissors') {
             document.getElementById('result').innerHTML = 'You lost';
+            cm_score++;
         }
     }
 
     else if (player == 'scissors') {
         if (computer == 'rock') {
             document.getElementById('result').innerHTML = 'You lost!';
+            cm_score++;
         }
         else if (player == 'paper') {
             document.getElementById('result').innerHTML = 'You won';
+            pl_score++;
         }
     }
 }
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+            playRound(button.id);
+            document.getElementById('score').innerHTML = pl_score + ' Vs ' + cm_score;
+
+            if (pl_score == 5) {
+                document.getElementById('result').innerHTML = 'YOU WON !!!';
+            }
+            else if (cm_score == 5) {
+                document.getElementById('result').innerHTML = 'YOU LOST !!!';
+            }
+    });
+});
